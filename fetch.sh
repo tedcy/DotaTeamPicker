8024df8a1b99ff41740df57c2b3befd786322504
tedcy <yue.cheng.ted@gmail.com>
2016-01-20 09:32:06 -0800

    add dotamax

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,8 @@
-curl http://192.168.52.128/fetchall/121160974
-curl http://192.168.52.128/fetchall/144727289
-curl http://192.168.52.128/fetchall/151207307
-curl http://192.168.52.128/fetchall/277772470
-curl http://192.168.52.128/fetchall/244850133
-curl http://192.168.52.128/fetchall/139483137
-curl http://192.168.52.128/fetchall/144725945
-curl http://192.168.52.128/fetchall/139432899
+curl http://192.168.52.128:8081/fetchall/121160974
+curl http://192.168.52.128:8081/fetchall/144727289
+curl http://192.168.52.128:8081/fetchall/151207307
+curl http://192.168.52.128:8081/fetchall/277772470
+curl http://192.168.52.128:8081/fetchall/244850133
+curl http://192.168.52.128:8081/fetchall/139483137
+curl http://192.168.52.128:8081/fetchall/144725945
+curl http://192.168.52.128:8081/fetchall/139432899
