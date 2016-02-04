402b2d0296270cce8e70151b14d2e76b17a8c35f
tedcy <yue.cheng.ted@gmail.com>
2016-02-03 18:50:18 -0800

    fix dev

--- picker/fetch_all_history.go
+++ picker/fetch_all_history.go
@@ -26,11 +26,14 @@ type matchInfo struct {
 	} `json:"result"`
 }
 
+var getMatchHistoryWithSeq = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001/?start_at_match_seq_num=1866720879"
+
 func FetchMatchsBySeq(seq int) {
 	
 }
 
 func (h *allHistory) fetchProcess(db *DB) {
+	//历史记录已经从sql里加载完毕
 	for {
 		if h.fetchSeqEnd == 0 {
 			nowMatchIds := FetchMatchsBySeq(0)
@@ -41,7 +44,20 @@ func (h *allHistory) fetchProcess(db *DB) {
 		//历史记录未抓取完整
 		if zeroSeq == 0 {
 			MatchIds := FetchMatchsBySeq(h.fetchSeqEnd)
+			//检查到686的时间戳
+			//如果达到时间戳，那么记录下该matchId
+			checkMatchReachZeroLine(MatchIds,zeroSeq)
+			SaveDb(DB,MatchIds)
 			h.fetchSeqEnd = nowMatchIds[len(MatchIds) - 1].Seq
+			continue
         }
+		//抓取最新记录
+		MatchIds := FetchMatchsBySeq(h.fetchSeqEnd)
+		//检查是否到zeroSeq
+		//如果达到，那么fetchSeqEnd = fetchSeqStart
+		checkMatchReachZeroLine(MatchIds,zeroSeq)
+		SaveDb(DB,MatchIds)
+		h.fetchSeqEnd = nowMatchIds[len(MatchIds) - 1].Seq
+		continue
 	}
 }
